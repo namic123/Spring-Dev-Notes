0d1fdf7339ec444c8b21ce5cae2b598fea089a92
jspark <namic123@naver.com>
2025-04-09 16:21:51 +0900

    feat: JPAQueryFactory를 스프링 빈으로 등록하여 DI 방식 개선
    - JPAQueryFactory를 @Bean으로 등록하여 구성 클래스에서 주입 가능하도록 변경
    - EntityManager는 프록시 객체로 주입되므로 동시성 문제 없이 안전하게 사용 가능
    - 관련 설명은 JPA 책 13.1 '트랜잭션 범위의 영속성 컨텍스트' 참고

--- querydsl/src/main/java/study/querydsl/QuerydslApplication.java
+++ querydsl/src/main/java/study/querydsl/QuerydslApplication.java
@@ -1,10 +1,19 @@
 package study.querydsl;
 
+import com.querydsl.jpa.impl.JPAQueryFactory;
+import jakarta.persistence.EntityManager;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.annotation.Bean;
 
 @SpringBootApplication
 public class QuerydslApplication {
+
+    // JPAQueryFactory 스프링 빈 등록
+    @Bean
+    JPAQueryFactory jpaQueryFactory(EntityManager em) {
+        return new JPAQueryFactory(em);
+    }
 
     public static void main(String[] args) {
         SpringApplication.run(QuerydslApplication.class, args);
