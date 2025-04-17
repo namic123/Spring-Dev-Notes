9f4951cca7d5c9547ca82ce95ee545e943832799
jspark <namic123@naver.com>
2025-04-17 17:47:02 +0900

    Spring security 활용 기본 프로젝트 생성

---
+++ spring-security-basic-utilize/src/main/java/spring/springsecuritybasicutilize/SpringSecurityBasicUtilizeApplication.java
@@ -0,0 +1,13 @@
+package spring.springsecuritybasicutilize;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringSecurityBasicUtilizeApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SpringSecurityBasicUtilizeApplication.class, args);
+	}
+
+}
---
+++ spring-security-basic-utilize/src/test/java/spring/springsecuritybasicutilize/SpringSecurityBasicUtilizeApplicationTests.java
@@ -0,0 +1,13 @@
+package spring.springsecuritybasicutilize;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpringSecurityBasicUtilizeApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
