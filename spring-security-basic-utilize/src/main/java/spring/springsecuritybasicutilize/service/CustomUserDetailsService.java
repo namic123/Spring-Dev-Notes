682c8a9a4e3cc5744401d8fe009bec46e181331c
jspark <namic123@naver.com>
2025-04-18 16:31:36 +0900

    오류 수정

--- spring-security-basic-utilize/src/main/java/spring/springsecuritybasicutilize/service/CustomUserDetailsService.java
+++ spring-security-basic-utilize/src/main/java/spring/springsecuritybasicutilize/service/CustomUserDetailsService.java
@@ -5,6 +5,7 @@ import org.springframework.security.core.userdetails.UserDetails;
 import org.springframework.security.core.userdetails.UserDetailsService;
 import org.springframework.security.core.userdetails.UsernameNotFoundException;
 import org.springframework.stereotype.Service;
+import spring.springsecuritybasicutilize.dto.CustomUserDetails;
 import spring.springsecuritybasicutilize.entity.UserEntity;
 import spring.springsecuritybasicutilize.repository.UserRepository;
 
