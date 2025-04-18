6f9528cbfe14291fe43da4cc1d53e0f8a8afd29a
jspark <namic123@naver.com>
2025-04-19 01:43:21 +0900

    feat: Http Basic 설명 md 파일 추가

--- spring-security-basic-utilize/src/main/java/spring/springsecuritybasicutilize/config/SecurityConfig.java
+++ spring-security-basic-utilize/src/main/java/spring/springsecuritybasicutilize/config/SecurityConfig.java
@@ -2,6 +2,7 @@ package spring.springsecuritybasicutilize.config;
 
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.security.config.Customizer;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
 import org.springframework.security.core.userdetails.User;
@@ -43,6 +44,9 @@ public class SecurityConfig {
                 .csrf((auth) -> auth.disable());
 
         // 구성된 SecurityFilterChain 반환
+
+        http
+                .httpBasic(Customizer.withDefaults());  // HTTP Basic 인증 활성화
 
         return http.build();
     }
