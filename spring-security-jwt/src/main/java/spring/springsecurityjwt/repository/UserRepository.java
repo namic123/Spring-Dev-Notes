474fca8c7ec773f5ea1efd2dce5953b2a4b24f49
jspark <namic123@naver.com>
2025-04-22 15:28:00 +0900

    feat: DB 기반 로그인 검증 로직 구현
    - UserRepository에 findByUsername() 메서드 추가
    - CustomUserDetailsService로 사용자 정보 로딩 로직 구현
    - CustomUserDetails 클래스에서 사용자 인증 정보 정의

--- spring-security-jwt/src/main/java/spring/springsecurityjwt/repository/UserRepository.java
+++ spring-security-jwt/src/main/java/spring/springsecurityjwt/repository/UserRepository.java
@@ -7,4 +7,7 @@ import spring.springsecurityjwt.entity.UserEntity;
 public interface UserRepository extends JpaRepository<UserEntity, Long> {
 
     Boolean existsByUsername(String username);
+
+    //username을 받아 DB 테이블에서 회원을 조회하는 메소드 작성
+    UserEntity findByUsername(String username);
 }
