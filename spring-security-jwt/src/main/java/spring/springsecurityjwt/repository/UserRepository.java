be567fc39d81760daf206023e6b1c2f7f8a89c8f
jspark <namic123@naver.com>
2025-04-22 15:04:38 +0900

    feat: CustomLoginFilter 기반 JWT 로그인 인증 흐름 구현
    - UsernamePasswordAuthenticationFilter 상속한 커스텀 필터 추가
    - SecurityConfig에 필터 등록 및 인증 관련 설정 구성
    - AuthenticationManager Bean 등록
    - 로그인 성공/실패 처리 메서드 구현 (JWT 발급 예정)

--- spring-security-jwt/src/main/java/spring/springsecurityjwt/repository/UserRepository.java
+++ spring-security-jwt/src/main/java/spring/springsecurityjwt/repository/UserRepository.java
@@ -2,8 +2,9 @@ package spring.springsecurityjwt.repository;
 
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.security.core.userdetails.User;
+import spring.springsecurityjwt.entity.UserEntity;
 
-public interface UserRepository extends JpaRepository<User, Long> {
+public interface UserRepository extends JpaRepository<UserEntity, Long> {
 
     Boolean existsByUsername(String username);
 }
