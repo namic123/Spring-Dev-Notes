feff932d7883ebb21a8953bc605f02d04be601a1
jspark <namic123@naver.com>
2025-04-25 14:17:23 +0900

    User Entity Lombok 애너테이션 분리 및 Builder 추가

--- spring-security-jwt/src/main/java/spring/springsecurityjwt/entity/UserEntity.java
+++ spring-security-jwt/src/main/java/spring/springsecurityjwt/entity/UserEntity.java
@@ -4,10 +4,13 @@ import jakarta.persistence.Entity;
 import jakarta.persistence.GeneratedValue;
 import jakarta.persistence.GenerationType;
 import jakarta.persistence.Id;
-import lombok.Data;
+import lombok.*;
 
 @Entity
-@Data
+@Getter @Setter
+@NoArgsConstructor
+@AllArgsConstructor
+@Builder
 public class UserEntity {
 
     @Id
