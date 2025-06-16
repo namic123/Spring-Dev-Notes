719cf1b6b12772e2650e7fb3cbac8e82a6170b1e
jspark <namic123@naver.com>
2025-06-16 15:27:51 +0900

    Swagger UI 예제 프로젝트 init

---
+++ swagger/src/main/java/spring/swagger/SwaggerApplication.java
@@ -0,0 +1,15 @@
+package spring.swagger;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.autoconfigure.security.servlet.SecurityAutoConfiguration;
+import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
+
+@SpringBootApplication(exclude = { SecurityAutoConfiguration.class })
+public class SwaggerApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(SwaggerApplication.class, args);
+    }
+
+}
