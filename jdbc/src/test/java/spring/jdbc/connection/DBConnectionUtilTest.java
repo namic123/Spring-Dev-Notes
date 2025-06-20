0fd83a9a7f9031680b55b9f52d2c1e176f37a609
jspark <namic123@naver.com>
2025-06-21 00:04:41 +0900

    커넥션 연결 테스트 기본 예제 만들기

---
+++ jdbc/src/test/java/spring/jdbc/connection/DBConnectionUtilTest.java
@@ -0,0 +1,18 @@
+package spring.jdbc.connection;
+
+import lombok.extern.slf4j.Slf4j;
+import org.junit.jupiter.api.Test;
+
+import java.sql.Connection;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+@Slf4j
+class DBConnectionUtilTest {
+
+    @Test
+    void connection(){
+        Connection connection = DBConnectionUtil.getConnection();
+        assertNotNull(connection);
+    }
+}
